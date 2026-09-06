67259abadaa4f4d6582342efb392dcd23a470049
Harsh Panchal <harsh220807@gmail.com>
2026-09-06 15:42:34 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"HARSH PANCHAL"<<endl;    //comments
+    cout<<"Harsh Panchal"<<endl;
+    int x = 6 , y = 4;
+    cout<<x<<"\n"<<"hello"<<endl<<y<<endl;
+    
+}
